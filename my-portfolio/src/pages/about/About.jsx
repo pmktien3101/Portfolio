2d4f9b102ec5af0df756f3439d6fa88efdbc1043
phamtien <phammackimtien@gmail.com>
2025-08-17 23:33:19 +0700

    [ADD] about page

--- my-portfolio/src/pages/about/About.jsx
+++ my-portfolio/src/pages/about/About.jsx
@@ -1,6 +1,40 @@
+import "./styles.css";
+
 const AboutComponent = () => {
     return (
-        <div>HEllo</div>
+        <div className="about-container">
+            <div className="left-content">
+                <img src="/img-portfolio.png" alt="Me" className="about-image"/>
+                <h3 className="left-name">Tina Pham</h3>   
+                <div className="left-major">Software Engineer Student</div> 
+                <ul className="left-social-links">
+                    <li className="left-social-item">
+                        <a href="https://github.com/pmktien3101" target="_blank"><img src="/GitHub.png" alt="GitHub" className="left-social-icon"/>
+                        </a>
+                    </li>
+                     <li className="left-social-item">
+                        <a href="https://github.com/pmktien3101" target="_blank"><img src="/LinkedIn.png" alt="LinkedIn" className="left-social-icon"/>
+                        </a>
+                    </li>
+                     <li className="left-social-item">
+                       <a href="https://github.com/pmktien3101" target="_blank"><img src="/Facebook.png" alt="Facebook" className="left-social-icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="right-content">
+                <div className="about-header">
+                    <ul>
+                      <li className="about-bio">Bio</li>
+                      <li className="about-education">Education</li>
+                      <li className="about-interests">Interests</li>
+                    </ul>
+                </div>
+                <div className="tabs-content">
+
+                </div>
+            </div>
+        </div>
     )
 }
 export default AboutComponent
