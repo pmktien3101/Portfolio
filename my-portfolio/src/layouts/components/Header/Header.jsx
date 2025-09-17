7b102fc4ce0875efe192031f3ab37e6e6079f587
phamtien <phammackimtien@gmail.com>
2025-09-17 21:30:02 +0700

    [FIX] add contact link

--- my-portfolio/src/layouts/components/Header/Header.jsx
+++ my-portfolio/src/layouts/components/Header/Header.jsx
@@ -12,10 +12,10 @@ const HeaderComponent = () => {
                         <NavLink to={ROUTE.HOME} className={({isActive}) => isActive ? "active" : ""}>Home</NavLink>  </li>
                     <li className="menu-item">
                         <NavLink to={ROUTE.ABOUT} className={({isActive}) => isActive ? "active" : ""}>About</NavLink>  </li>
+                     <li className="menu-item">
+                        <NavLink to={ROUTE.SKILL} className={({isActive}) => isActive ? "active" : ""}>Projects</NavLink>  </li>   
                     <li className="menu-item">
-                        <NavLink to={ROUTE.PROJECT} className={({isActive}) => isActive ? "active" : ""}>Projects</NavLink>  </li>
-                    <li className="menu-item">
-                        <NavLink to={ROUTE.SKILL} className={({isActive}) => isActive ? "active" : ""}>Skills</NavLink>  </li>
+                        <NavLink to={ROUTE.PROJECT} className={({isActive}) => isActive ? "active" : ""}>Contact</NavLink>  </li>
                 </ul>
             </div>
     )
